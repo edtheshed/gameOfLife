c9fdf66086ae5097ca069109734e9f6009de4c0f
edtheshed <edward.rixon@codurance.com>
2019-10-31 14:23:41 +0000

    refactored iterate to reduce indentation

--- src/main/java/GameOfLife.java
+++ src/main/java/GameOfLife.java
@@ -4,13 +4,21 @@ public class GameOfLife {
 
     public boolean[][] play(boolean[][] board) {
         this.board = board;
-        for (int row = 0; row < this.board.length; row++) {
-            for (int column = 0; column < this.board[row].length; column++) {
-                killCell(row, column);
-            }
-        }
-       this.board[0][1] = true;
+        iterateBoardToKillCells();
+        this.board[0][1] = true;
         return this.board;
+    }
+
+    private void iterateBoardToKillCells() {
+        for (int row = 0; row < this.board.length; row++) {
+            iterateColumns(row);
+        }
+    }
+
+    private void iterateColumns(int row) {
+        for (int column = 0; column < this.board[row].length; column++) {
+            killCell(row, column);
+        }
     }
 
     private void killCell(int row, int column) {
