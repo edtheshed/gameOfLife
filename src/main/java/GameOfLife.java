d204750cf4444e70af99cb3900724f5029c661b2
edtheshed <edward.rixon@codurance.com>
2019-10-31 14:18:58 +0000

    added double for loop to iterate through cells

--- src/main/java/GameOfLife.java
+++ src/main/java/GameOfLife.java
@@ -1,6 +1,11 @@
 public class GameOfLife {
     public boolean[][] play(boolean[][] board) {
-        board[0][2] = false;
+        for (int row = 0; row < board.length; row++) {
+            for (int column = 0; column < board[row].length; column++) {
+                board[row][column] = false;
+            }
+        }
+        board[0][1] = true;
         return board;
     }
 }
